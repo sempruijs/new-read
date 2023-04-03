6a0ea3fc13b8ae076f9245fd4290d4c9f737191d
sempruijs <sem@pruijs.net>
2023-04-03 15:39:54 +0200

    ignore target

--- conversion/v1/target/debug/deps/v1-10a6d97f5396706a.d
+++ conversion/v1/target/debug/deps/v1-10a6d97f5396706a.d
@@ -1,5 +1,5 @@
-/Users/sem/dev/new-read/conversion/rust/v1/target/debug/deps/v1-10a6d97f5396706a.rmeta: src/lib.rs
+/Users/sem/dev/new-read/conversion/v1/target/debug/deps/v1-10a6d97f5396706a.rmeta: src/lib.rs
 
-/Users/sem/dev/new-read/conversion/rust/v1/target/debug/deps/v1-10a6d97f5396706a.d: src/lib.rs
+/Users/sem/dev/new-read/conversion/v1/target/debug/deps/v1-10a6d97f5396706a.d: src/lib.rs
 
 src/lib.rs:
--- conversion/v1/target/debug/deps/v1-d0c68147a6fabb89.d
+++ conversion/v1/target/debug/deps/v1-d0c68147a6fabb89.d
@@ -1,7 +1,7 @@
-/Users/sem/dev/new-read/conversion/rust/v1/target/debug/deps/v1-d0c68147a6fabb89.rmeta: src/lib.rs
+/Users/sem/dev/new-read/conversion/v1/target/debug/deps/v1-d0c68147a6fabb89.rmeta: src/lib.rs
 
-/Users/sem/dev/new-read/conversion/rust/v1/target/debug/deps/libv1-d0c68147a6fabb89.rlib: src/lib.rs
+/Users/sem/dev/new-read/conversion/v1/target/debug/deps/libv1-d0c68147a6fabb89.rlib: src/lib.rs
 
-/Users/sem/dev/new-read/conversion/rust/v1/target/debug/deps/v1-d0c68147a6fabb89.d: src/lib.rs
+/Users/sem/dev/new-read/conversion/v1/target/debug/deps/v1-d0c68147a6fabb89.d: src/lib.rs
 
 src/lib.rs:
