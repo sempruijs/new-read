e6d10f76a8a9e9b119ed97ab618d5a16ac789fba
sempruijs <sem@pruijs.net>
2023-04-04 20:55:40 +0200

    move rules tests to own module

--- conversion/v1/src/lib.rs
+++ conversion/v1/src/lib.rs
@@ -172,11 +172,13 @@ fn convert_word(word: &str) -> String {
 }
 
 pub fn convert_string(content: &str) -> String {
+    //apply new-read to every word
     let result: String = content
         .split(' ')
         .map(|word| convert_word(word) + " ")
         .collect::<String>();
 
+    //remove last space from the string
     let mut result = result.chars();
     result.next_back();
     result.as_str().to_string()
@@ -222,113 +224,117 @@ mod tests {
         }
     }
 
-    #[test]
-    fn test_r1() {
-        let example = Example::build("chocola", "sjocola");
+    mod test_rules {
+        use crate::tests::*;
 
-        assert!(test_conversion(rules::r1, &example));
-    }
+        #[test]
+        fn test_r1() {
+            let example = Example::build("chocola", "sjocola");
 
-    #[test]
-    fn test_r2() {
-        let example_1 = Example::build("ijsje", "ysje");
-        let example_2 = Example::build("belangrijk", "belangryk");
+            assert!(test_conversion(rules::r1, &example));
+        }
 
-        assert!(test_conversion(rules::r2, &example_1));
-        assert!(test_conversion(rules::r2, &example_2));
-    }
+        #[test]
+        fn test_r2() {
+            let example_1 = Example::build("ijsje", "ysje");
+            let example_2 = Example::build("belangrijk", "belangryk");
 
-    #[test]
-    fn test_r3() {
-        let example_1 = Example::build("eindelijk", "yndelijk");
-        let example_2 = Example::build("eisen", "ysen");
+            assert!(test_conversion(rules::r2, &example_1));
+            assert!(test_conversion(rules::r2, &example_2));
+        }
 
-        assert!(test_conversion(rules::r3, &example_1));
-        assert!(test_conversion(rules::r3, &example_2));
-    }
+        #[test]
+        fn test_r3() {
+            let example_1 = Example::build("eindelijk", "yndelijk");
+            let example_2 = Example::build("eisen", "ysen");
 
-    #[test]
-    fn test_r4() {
-        let example_1 = Example::build("canon", "kanon");
-        let example_2 = Example::build("cacoa", "kakoa");
+            assert!(test_conversion(rules::r3, &example_1));
+            assert!(test_conversion(rules::r3, &example_2));
+        }
 
-        assert!(test_conversion(rules::r4, &example_1));
-        assert!(test_conversion(rules::r4, &example_2));
-    }
+        #[test]
+        fn test_r4() {
+            let example_1 = Example::build("canon", "kanon");
+            let example_2 = Example::build("cacoa", "kakoa");
 
-    #[test]
-    fn test_r5() {
-        let example_1 = Example::build("quiz", "kwuiz");
+            assert!(test_conversion(rules::r4, &example_1));
+            assert!(test_conversion(rules::r4, &example_2));
+        }
 
-        assert!(test_conversion(rules::r5, &example_1));
-    }
+        #[test]
+        fn test_r5() {
+            let example_1 = Example::build("quiz", "kwuiz");
 
-    #[test]
-    fn test_r6() {
-        let example_1 = Example::build("fiets", "viets");
-        let example_2 = Example::build("fluit", "vluit");
+            assert!(test_conversion(rules::r5, &example_1));
+        }
 
-        assert!(test_conversion(rules::r6, &example_1));
-        assert!(test_conversion(rules::r6, &example_2));
-    }
+        #[test]
+        fn test_r6() {
+            let example_1 = Example::build("fiets", "viets");
+            let example_2 = Example::build("fluit", "vluit");
 
-    #[test]
-    fn test_r7() {
-        let example_1 = Example::build("school", "sgool");
-        let example_2 = Example::build("schoon", "sgoon");
+            assert!(test_conversion(rules::r6, &example_1));
+            assert!(test_conversion(rules::r6, &example_2));
+        }
 
-        assert!(test_conversion(rules::r7, &example_1));
-        assert!(test_conversion(rules::r7, &example_2));
-    }
+        #[test]
+        fn test_r7() {
+            let example_1 = Example::build("school", "sgool");
+            let example_2 = Example::build("schoon", "sgoon");
 
-    #[test]
-    fn test_r8() {
-        let example_1 = Example::build("wordt", "wort");
+            assert!(test_conversion(rules::r7, &example_1));
+            assert!(test_conversion(rules::r7, &example_2));
+        }
 
-        assert!(test_conversion(rules::r8, &example_1));
-    }
+        #[test]
+        fn test_r8() {
+            let example_1 = Example::build("wordt", "wort");
 
-    #[test]
-    fn test_r9() {
-        let example_1 = Example::build("tijd", "tijt");
-        let example_2 = Example::build("deed", "deet");
+            assert!(test_conversion(rules::r8, &example_1));
+        }
 
-        assert!(test_conversion(rules::r9, &example_1));
-        assert!(test_conversion(rules::r9, &example_2));
-    }
+        #[test]
+        fn test_r9() {
+            let example_1 = Example::build("tijd", "tijt");
+            let example_2 = Example::build("deed", "deet");
 
-    #[test]
-    fn test_r10() {
-        let example_1 = Example::build("ring", "riq");
-        let example_2 = Example::build("bang", "baq");
+            assert!(test_conversion(rules::r9, &example_1));
+            assert!(test_conversion(rules::r9, &example_2));
+        }
 
-        assert!(test_conversion(rules::r10, &example_1));
-        assert!(test_conversion(rules::r10, &example_2));
-    }
+        #[test]
+        fn test_r10() {
+            let example_1 = Example::build("ring", "riq");
+            let example_2 = Example::build("bang", "baq");
 
-    #[test]
-    fn test_r11() {
-        let example_1 = Example::build("bankier", "baqkier");
-        let example_2 = Example::build("bedanken", "bedaqken");
+            assert!(test_conversion(rules::r10, &example_1));
+            assert!(test_conversion(rules::r10, &example_2));
+        }
 
-        assert!(test_conversion(rules::r11, &example_1));
-        assert!(test_conversion(rules::r11, &example_2));
-    }
+        #[test]
+        fn test_r11() {
+            let example_1 = Example::build("bankier", "baqkier");
+            let example_2 = Example::build("bedanken", "bedaqken");
 
-    #[test]
-    fn test_r12() {
-        let example_1 = Example::build("ouderwets", "auderwets");
-        let example_2 = Example::build("oude", "aude");
+            assert!(test_conversion(rules::r11, &example_1));
+            assert!(test_conversion(rules::r11, &example_2));
+        }
 
-        assert!(test_conversion(rules::r12, &example_1));
-        assert!(test_conversion(rules::r12, &example_2));
-    }
+        #[test]
+        fn test_r12() {
+            let example_1 = Example::build("ouderwets", "auderwets");
+            let example_2 = Example::build("oude", "aude");
 
-    #[test]
-    fn test_r13() {
-        let example_1 = Example::build("extra", "ekstra");
+            assert!(test_conversion(rules::r12, &example_1));
+            assert!(test_conversion(rules::r12, &example_2));
+        }
 
-        assert!(test_conversion(rules::r13, &example_1));
+        #[test]
+        fn test_r13() {
+            let example_1 = Example::build("extra", "ekstra");
+
+            assert!(test_conversion(rules::r13, &example_1));
+        }
     }
 
     #[test]
